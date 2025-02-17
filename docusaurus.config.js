7153e5ea9a33e3fc8b2437de8491923000df92d9
Abraham Neuwirth <abeneuwirth@gmail.com>
2025-02-17 10:41:56 -0500

    correct settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,8 +22,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "prg2501", // Usually your GitHub org/user name.
+  projectName: "documentation", // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
